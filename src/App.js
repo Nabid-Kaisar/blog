fbc8661157bfb1a9cbf6b0fe69cd145dc33659ed
Kaisar <himelkaisar023@gmail.com>
2018-10-11 14:56:31 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from "./Header.js"
 import SearchBarTop from "./SearchBarTop"
 import './App.css';
+import NavBar from "./NavBar"
 
 class App extends Component {
   render() {
@@ -10,6 +11,9 @@ class App extends Component {
       <div className="titleCompContainer">
         <Header />
         <SearchBarTop />
+      </div>
+      <div className="navBar">
+      <NavBar/>
       </div>
       </div>
     );
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import "./NavBarStyle.css";
 
 class NavBar extends Component {
   render() {
     return (
       <div>
-
+        <div className="topnav">
+          <a href="#home" className ="home">Home</a>
+          <a href="#category">Category</a>
+          <a href="#post">Post Now</a>
+          <a href="#about">About</a>
+        </div>
       </div>
     );
   }
